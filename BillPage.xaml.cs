d1ed77786aad6512fbb9485d46149f539b07c6bd
DESKTOP-BDLU14E\user <emanuelignat@yahoo.com>
2023-08-11 00:35:33 +0300

    Save bill functionality

--- BillPage.xaml.cs
+++ BillPage.xaml.cs
@@ -1,6 +1,8 @@
 using MAUI_App_Tutorial.Models;
 using Mopups.Services;
 using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text;
 
 namespace MAUI_App_Tutorial;
 
@@ -58,6 +60,29 @@ public partial class BillPage : ContentPage
             await Navigation.PushAsync(new BillItemDetails(billItem, BillItemsList, this.user, this.table));
         }
         billItemsUI.SelectedItem = null;
+    }
+
+    public async void OnSendButtonClicked(object sender, EventArgs e)
+    {
+        var unsentBillItems = BillItemsList.Where(billItem => billItem.orderSent == false).ToList();
+
+        string baseURL = Preferences.Get("BaseURL", "");
+        var endpoint = $"{baseURL}/api/BillItems/saveTableBill/{this.table.Id}?waiterName={this.user.NumeUtilizator}";
+
+        var json = JsonSerializer.Serialize<List<BillItem>>(unsentBillItems);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        HttpResponseMessage response = await this.httpClient
+            .PutAsync(endpoint, content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var tableId = await response.Content.ReadFromJsonAsync<long>();
+            await DisplayAlert("Succes", "Comanda a fost trimisa cu succes!", "Ok");
+            await Navigation.PopAsync();
+        } else
+        {
+            await DisplayAlert("Eroare", "Ceva nu a mers bine!", "Ok");
+        }
     }
 
     public void OnHomeButtonClicked(object sender, EventArgs e)
